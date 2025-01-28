d52becf2f0023d93fa7c7a2d0d1e61c27336d485
AQUATECHIE <amolequwam96@gmail.com>
2025-01-28 12:27:42 +0100

    GG

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require("config")
 const {check, validationResult} = require('express-validator');
 
 const User = require('../../models/Users')
@@ -58,9 +60,17 @@ async (req, res) => {
 
         await user.save();
     
-        // return jsonwebtoken
-
-        res.send('User registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        jwt.sign(payload, 
+        config.get('jwtSecret'),{expiresIn: 36000},
+        (err, token)=>{
+            if(err) throw err;
+            res.json({ token });
+        } )
 
     } catch(err) {
         console.error(err.message);
